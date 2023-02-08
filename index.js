509b8a8693033811edb277f64622a4e2c0cd85b3
chparmar10@gmail.com <chparmar10@gmail.com>
2023-02-08 12:09:00 +0530

    product crud operation

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require('cors');
 const User = require("./db/users");
 require("./db/config");
+const Products = require("./db/Product");
+const Product = require("./db/Product");
 //const mongoose = require("mongoose");
  
 const app = express();
@@ -47,4 +49,65 @@ app.get("/",(req,res)=>{
     res.send("app is connected");
 });
 
-app.listen(4300)
+
+/// Products APIS
+
+app.post("/add-product",async(req,res)=>{
+    let product = new Products(req.body);
+    let result = await product.save();
+    res.send(result);
+});
+
+
+app.get("/products",async (req,res)=>{
+    let products = await Products.find({});
+    if(products.length > 0){
+        res.send(products);
+    }
+    else{
+        res.send({result:"Products Not Found"});
+    }
+});
+
+app.delete("/product/:id",async(req,res)=>{
+    console.warn("id : ",req.params.id);
+    let result = await Product.deleteOne({_id:req.params.id});
+    res.send(result);
+});
+
+
+app.get("/product/:id",async(req,res)=>{
+    let result = await Product.findOne({_id:req.params.id});
+    if(result){
+        res.send(result);
+    }
+    else{
+        res.send({result: "No Records Found"});
+    }
+});
+
+
+app.put("/product/:id",async(req,res)=>{
+    let result = await Product.updateOne(
+        {_id:req.params.id},
+        {
+            $set:req.body
+        }
+    );
+    res.send(result);
+});
+
+
+app.get("/search/:key",async(req,res)=>{
+    let result = await Product.find({
+        "$or":[
+            {name:{$regex:req.params.key}},
+            {price:{$regex:req.params.key}},
+            {category:{$regex:req.params.key}},
+            {company:{$regex:req.params.key}},
+        ]
+    });
+    res.send(result);
+});
+
+app.listen(4300);
